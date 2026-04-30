651d553e52245eb4f00c4e6b0395154f1068f8ec
0xBrian19722 <brian.tec19722@gmail.com>
2026-04-30 01:01:24 -0300

    Actualizacion

---
+++ Funciones/Ejercicio13.py
@@ -0,0 +1,13 @@
+
+
+
+def numero_entero():
+    numero = input("Ingrese un numero: ")
+    while not numero.isdigit():   
+        numero = input("ERROR: Ingrese nuevamente el numero: ")   #Ejercicio 1 con validacion 
+    print (f"El numero es: {numero}")           
+
+# numero_entero()
+
+
+
